8c284e6974a78737589c33a34bb382f790003486
封晔 <578797371@qq.com>
2017-12-04 18:02:22 +0800

    TreeView

---
+++ Wpf.Train.UI/ViewModels/TreeViewListViewModel.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Wpf.Train.UI
+{
+    public class TreeViewListViewModel : ViewModelBase
+    {
+        public TreeViewListViewModel()
+        {
+            node = string.Empty;
+            isChecked = false;
+            nodeName = string.Empty;
+            ParentNode = null;
+            NodeData = null;
+            ChildrenList = new ObservableCollection<TreeViewListViewModel>();
+
+            isNodeExpanded = true;
+            nodeImage = "\ue613";
+        }
+
+        private string node;
+        /// <summary>
+        /// 结点编号
+        /// </summary>
+        public string Node
+        {
+            get { return node; }
+            set
+            {
+                if (value != this.Node)
+                {
+                    node = value;
+                    OnPropertyChanged(() => this.Node);
+                }
+            }
+        }
+
+        private bool isChecked;
+        /// <summary>
+        /// 是否选中
+        /// </summary>
+        public bool IsChecked
+        {
+            get { return isChecked; }
+            set
+            {
+                if (value != this.IsChecked)
+                {
+                    isChecked = value;
+                    OnPropertyChanged(() => this.IsChecked);
+
+                    if (isChecked)
+                    {
+                        CheckAllNode(ChildrenList, true);
+
+                        //说明时根结点
+                        if (this.ParentNode == null)
+                            return;
+
+                        if (this.ParentNode.ChildrenList.Count > 0)
+                        {
+                            bool isChildChecked1 = false;
+                            foreach (var item in this.ParentNode.ChildrenList)
+                            {
+                                if (item.IsChecked == false)
+                                {
+                                    isChildChecked1 = true;
+                                    break;
+                                }
+                            }
+
+                            //如果全部选中
+                            if (isChildChecked1 == false)
+                            {
+                                //如果不是根结点
+                                if (this.ParentNode != null)
+                                {
+                                    if (this.ParentNode.IsChecked == false)
+                                    {
+                                        this.ParentNode.IsChecked = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //先取消所有子节点的选中
+                        CheckAllNode(ChildrenList, false);
+
+                        //说明时根结点
+                        if (this.ParentNode == null)
+                            return;
+
+                        if (this.ParentNode.ChildrenList.Count > 0)
+                        {
+                            bool isChildChecked = false;
+                            foreach (var item in this.ParentNode.ChildrenList)
+                            {
+                                if (item.IsChecked == true)
+                                {
+                                    isChildChecked = true;
+                                    break;
+                                }
+                            }
+
+                            //如果全部子结点没有选择的话
+                            if (!isChildChecked)
+                            {
+                                if (this.ParentNode.IsChecked == true)
+                                {
+                                    this.ParentNode.IsChecked = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CheckAllNode(ObservableCollection<TreeViewListViewModel> treeList, bool isChecked)
+        {
+            foreach (var item in treeList)
+            {
+                item.IsChecked = isChecked;
+                CheckAllNode(item.ChildrenList, isChecked);
+            }
+        }
+
+        private string nodeImage;
+        /// <summary>
+        /// 结点图片
+        /// </summary>
+        public string NodeImage
+        {
+            get { return nodeImage; }
+            set
+            {
+                if (value != this.nodeImage)
+                {
+                    nodeImage = value;
+                    OnPropertyChanged(() => this.NodeImage);
+                }
+            }
+        }
+
+        private string nodeName;
+        /// <summary>
+        /// 结点名称
+        /// </summary>
+        public string NodeName
+        {
+            get { return nodeName; }
+            set
+            {
+                if (value != this.nodeName)
+                {
+                    nodeName = value;
+                    OnPropertyChanged(() => this.NodeName);
+                }
+            }
+        }
+
+        private bool isNodeExpanded;
+        /// <summary>
+        /// 结点是否展开
+        /// </summary>
+        public bool IsNodeExpanded
+        {
+            get { return isNodeExpanded; }
+            set
+            {
+                if (value != this.isNodeExpanded)
+                {
+                    isNodeExpanded = value;
+                    OnPropertyChanged(() => this.isNodeExpanded);
+
+                    if (this.ChildrenList.Count <= 0)
+                        return;
+
+                    //if (isNodeExpanded)
+                    //{
+                    //    this.NodeImage = "\ue613";
+                    //}
+                    //else
+                    //{
+                    //    this.NodeImage = "\ue613";
+                    //}
+                }
+            }
+        }
+
+        /// <summary>
+        /// 父结点
+        /// </summary>
+        public TreeViewListViewModel ParentNode { get; set; }
+        public Object NodeData { get; set; }
+        public ObservableCollection<TreeViewListViewModel> ChildrenList { get; set; }
+    }
+}
--- Wpf.Train.UI/Views/Messages/Messages.xaml.cs
+++ Wpf.Train.UI/Views/Messages/Messages.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf.Train.CustomControlLib;
+using Wpf.Train.Entiry;
 
 namespace Wpf.Train.UI
 {
@@ -21,9 +23,24 @@ namespace Wpf.Train.UI
     /// </summary>
     public partial class Messages : PageBase
     {
+
+        public string MyTestProperty
+        {
+            get { return (string)GetValue(MyTestPropertyProperty); }
+            set { SetValue(MyTestPropertyProperty, value); }
+        }
+
+        public static readonly DependencyProperty MyTestPropertyProperty =
+            DependencyProperty.Register("MyTestProperty", typeof(string), typeof(Messages), new PropertyMetadata("测试依赖属性"));
+
+
+
+        public ObservableCollection<TreeViewListViewModel> UserTreeList { get; set; }
         public Messages()
         {
             InitializeComponent();
+            UserTreeList = new ObservableCollection<TreeViewListViewModel>();
+            txt_testTxt.DataContext = MyTestProperty;
         }
 
         private void PageBase_Loaded(object sender, RoutedEventArgs e)
@@ -34,6 +51,84 @@ namespace Wpf.Train.UI
             }
             var list = new List<string>() { "1", "2", "3", "4" };
             exCom.ItemsSource = list;
+
+            InitUserTreeList();
+        }
+
+        private void InitUserTreeList()
+        {
+            //添加根结点
+            var org = new Organization()
+            {
+                 Id = "1",
+                 Name = "组织架构"
+            };
+            var rootModel = new TreeViewListViewModel()
+            {
+                Node = org.Id,
+                NodeName = org.Name,
+                NodeImage = "\ue613",
+                NodeData = org
+            };
+            UserTreeList.Add(rootModel);
+            //添加子结点1
+            var teams = new Teams()
+            {
+                Id = "1",
+                Name = "小组1"
+            };
+            var teamModel = new TreeViewListViewModel()
+            {
+                Node = teams.Id,
+                NodeName = teams.Name,
+                NodeImage = "\ue611",
+                NodeData = org
+            };
+            UserTreeList[0].ChildrenList.Add(teamModel);
+            //添加子结点2
+            var teams1 = new Teams()
+            {
+                Id = "2",
+                Name = "小组2"
+            };
+            var teamModel1 = new TreeViewListViewModel()
+            {
+                Node = teams1.Id,
+                NodeName = teams1.Name,
+                NodeImage = "\ue611",
+                NodeData = org
+            };
+            //给结点2添加用户
+            var users = new User()
+            {
+                Id ="1",
+                Name = "用户1"
+            };
+            var teamModel2 = new TreeViewListViewModel()
+            {
+                Node = users.Id,
+                NodeName = users.Name,
+                NodeImage = "\ue656",
+                NodeData = org
+            };
+            var users1 = new User()
+            {
+                Id = "2",
+                Name = "用户2"
+            };
+            var teamModel3 = new TreeViewListViewModel()
+            {
+                Node = users1.Id,
+                NodeName = users1.Name,
+                NodeImage = "\ue656",
+                NodeData = org
+            };
+            teamModel1.ChildrenList.Add(teamModel2);
+            teamModel1.ChildrenList.Add(teamModel3);
+            UserTreeList[0].ChildrenList.Add(teamModel1);
+            
+
+            tv_totalPersonList.ItemsSource = UserTreeList;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -76,6 +171,28 @@ namespace Wpf.Train.UI
             {
                 MessageBox.Show("false");
             }
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            var rootModel = new TreeViewListViewModel()
+            {
+                Node = "2",
+                NodeName = "添加结点",
+                NodeImage = "\ue611",
+            };
+            UserTreeList[0].ChildrenList.Add(rootModel);
+        }
+
+        private void btn_Re_Click(object sender, RoutedEventArgs e)
+        {
+            UserTreeList.Clear();
+            InitUserTreeList();
+        }
+
+        private void btn_ch_Click(object sender, RoutedEventArgs e)
+        {
+            MyTestProperty = "改变";
         }
     }
 }
